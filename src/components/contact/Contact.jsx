5cb4f7864e1383c6338096fddb1168e32ca1e822
Andrea Chirillano <andreatchirillano@hotmail.com>
2024-03-17 16:47:12 -0300

    Fix: send email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,59 +1,62 @@
 import "./Contact.css";
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
 
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
+        from_name: '',
+        from_email: '',
         message: ''
     });
 
     const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        });
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
     };
 
-    const handleSubmit = (e) => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_6j4gd5h', 'template_i0ymcon', e.target, 'W5qpAwNWAmFDClrde')
+        emailjs.sendForm('service_6j4gd5h', 'template_i0ymcon', form.current, { publicKey: 'W5qpAwNWAmFDClrde' })
             .then((result) => {
                 console.log(result.text);
                 alert('Message sent successfully');
-                setFormData({
-                    name: '',
-                    email: '',
-                    message: ''
-                });
             }, (error) => {
                 console.log(error.text);
                 alert('There was an error sending the message');
             });
+        setFormData({
+            from_name: '',
+            from_email: '',
+            message: ''
+        });
     };
 
     return (
         <div className="contact-background" id="contact">
             <div className="contact-form">
-            <h2>Contact me</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
-                </div>
-                <button type="submit">Send message</button>
-            </form>
+                <h2>Contact me</h2>
+                <form ref={form} onSubmit={sendEmail}>
+                    <div>
+                        <label htmlFor="from_name">Name:</label>
+                        <input type="text" id="from_name" name="from_name" value={formData.from_name} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label htmlFor="from_email">Email:</label>
+                        <input type="email" id="from_email" name="from_email" value={formData.from_email} onChange={handleChange} required />
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message:</label>
+                        <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
+                    </div>
+                    <button type="submit" value="Send">Send message</button>
+                </form>
             </div>
         </div>
 
