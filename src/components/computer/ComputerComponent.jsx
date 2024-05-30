aaf59c83f0c70ff840c3ca70f91e39170dfc4a18
Andrea Chirillano <andreatchirillano@hotmail.com>
2024-05-30 14:38:20 -0300

    Add: hooks

--- src/components/computer/ComputerComponent.jsx
+++ src/components/computer/ComputerComponent.jsx
@@ -3,25 +3,30 @@ import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei'; 
 import Computer from '../../../public/Computer';
+import useMediaQuery from './useMediaQuery';
 
 const ComputerComponent = () => {
+    const isScreenSmall = useMediaQuery('(max-width: 700px)');
+
+    const cameraPosition = isScreenSmall ? [10, 5, 10] : [5, 1, 5];
+
     return (
         <div className="computer-background">
             <Canvas className="computer">
                 <ambientLight />
                 <PerspectiveCamera 
                     makeDefault 
-                    position={[5, 2, 5]} 
+                    position={cameraPosition}
                     rotation={[0, -Math.PI / 4, 0]} 
                 />
                 <OrbitControls 
-                    enableZoom={false} 
-                    target={[0, 0, 0]} // Focus on the center of the computer
+                    enableZoom={false}
+                    target={[0, 0, 0]}
                 />
                 <Suspense fallback={null}>
                     <Computer />
                 </Suspense>
-                <Environment preset='sunset'/>
+                <Environment preset="sunset" />
             </Canvas>
         </div>
     );
